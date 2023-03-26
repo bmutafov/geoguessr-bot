3aa698ec166011ae2b32506fdcb9cc185ed35793
Boris Mutafov <bmutafov@gmail.com>
2023-03-26 18:58:50 +0300

    feat: make image cool looking

--- src/challenges/results-image.ts
+++ src/challenges/results-image.ts
@@ -62,42 +62,51 @@ function getPlayersResults(players: ChallengePlayerScore[]) {
 export async function resultsImage(token: string) {
 	const { data } = await geoGuessrClient<ChallengeResultsResponse>(getResultUrl(token));
 	const playerResults = getPlayersResults(data.items);
-
 	const result = await nodeHtmlToImage({
-		html: `
-    <html>
-    <head>
-      <style>
-      body {
-        height: 200px;
-        width: 300px;
-      }
+		html: `<style>
+		@import url("https://fonts.cdnfonts.com/css/neo-sans-pro"); table { font-family: "Neo Sans Pro",
+		sans-serif; background: rgb(2, 0, 36); background: linear-gradient( 128deg, rgba(2, 0, 36, 1) 0%,
+		rgba(9, 9, 121, 1) 47%, rgba(146, 20, 255, 1) 100% ); padding: 1rem; border-radius: 20px; color:
+		white; } th, td { text-align: left; padding: 2px 15px; } th { padding-bottom: 10px; } .player-info
+		{ display: flex; align-items: center; gap: 10px; } .player-info img { border: 3px solid white;
+		border-radius: 50%; }
 
-      table { 
-        color: orange;
-      }
-      </style>
-    </head>
-    <body
-      <table>
-        {{#each playerResults}}
-          <tr>
-            <td>
-              <img src="{{avatar}}" width="48" height="48" />
-            </td>
-            <td>{{playerName}}</td>
-            {{#each guesses}}
-              <td>{{this}}</td>
-            {{/each}}
-            <td>{{totalScore}}</td>
-          </tr>
-        {{/each}}
-      </table>
-    </body>
-    </html>
-      `,
-		content: { playerResults },
+		.container { height: {{containerHeight}}px, width: 1300px }
+	</style>
+	<div class='container'>
+		<table>
+			<thead>
+				<tr>
+					<th>Player</th>
+					<th>Round 1</th>
+					<th>Round 2</th>
+					<th>Round 3</th>
+					<th>Round 4</th>
+					<th>Round 5</th>
+					<th>Total Score</th>
+				</tr>
+			</thead>
+			<tbody>
+				{{#each playerResults}}
+					<tr>
+						<td>
+							<div class='player-info'>
+								<img src='{{avatar}}' width='24' height='24' />
+								{{playerName}}
+							</div>
+						</td>
+						{{#each guesses}}
+							<td>{{this}}</td>
+						{{/each}}
+						<td>{{totalScore}}</td>
+					</tr>
+				{{/each}}
+			</tbody>
+		</table>
+	</div>`,
+		content: { containerHeight: playerResults.length * 35 + 70, playerResults },
 		transparent: true,
+		selector: '.container',
 	});
 
 	return result;
